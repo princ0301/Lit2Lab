8f54cfae8b1ccb53cf277a2b6c6615b338fad045
princ0301 <prince07190@gmail.com>
2026-03-02 00:47:33 +0530

    kernel detectio automatic venv creation

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,7 +2,7 @@ from tools.pdf_parser import parse_pdf
 from tools.script_builder import save_script, load_script
 from tools.script_runner import run_script
 from tools.py_to_notebook import py_to_notebook
-from tools.kernel_detector import detect_best_kernel, find_venv_in_current_dir, get_available_kernels
+from tools.kernel_detector import detect_best_kernel, get_agent_python
 
 __all__ = [
     "parse_pdf",
@@ -11,6 +11,5 @@ __all__ = [
     "run_script",
     "py_to_notebook",
     "detect_best_kernel",
-    "find_venv_in_current_dir",
-    "get_available_kernels",
+    "get_agent_python",
 ]
